e1c43d9ae3319244dd9ac3fb29459439bba053fc
mikekidder <mikekidder@gmail.com>
2016-03-13 18:37:21 -0500

    application adjustments

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -22,12 +24,12 @@ System.register(['angular2/core'], function(exports_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App -- BETA 1</h1>'
+                        template: '<h1>My First Angular 2 App</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
-System.register(['angular2/platform/browser', './app.component'], function(exports_1) {
+System.register(['angular2/platform/browser', './app.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var browser_1, app_component_1;
     return {
         setters:[
@@ -13,4 +15,4 @@ System.register(['angular2/platform/browser', './app.component'], function(expor
         }
     }
 });
-//# sourceMappingURL=boot.js.map
+//# sourceMappingURL=main.js.map
